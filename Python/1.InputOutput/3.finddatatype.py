20d725aa5d5b9f373ffc567717551ee2e4d3bb7a
Roshan Palaniappan <138195422+RODEEKS@users.noreply.github.com>
2024-01-03 19:39:50 +0530

    Add files via upload

---
+++ Python/1.InputOutput/3.finddatatype.py
@@ -0,0 +1,14 @@
+
+#type() is used to find the datatype of the particuler variable 
+
+a=10
+print(a)
+print(type(a))
+
+a=2.5
+print(a)
+print(type(a))
+
+a='roshan'
+print(a)
+print(type(a))
---
+++ Python/1.InputOutput/4.takinginput.py
@@ -0,0 +1,9 @@
+a=int(input('enter a age: '))
+print('the value of a is :' , a,' and the type of a is :',type(a))
+
+a=input("what is ur name: ")
+print('my name is: ',a)
+
+a=int(input())
+b=int(input())
+print(a + b)
