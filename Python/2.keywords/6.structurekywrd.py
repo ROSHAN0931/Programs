f16bc929688f896a5c557c6ee5c153d28f913f08
Roshan Palaniappan <138195422+RODEEKS@users.noreply.github.com>
2024-01-05 11:15:06 +0530

    6.structurekywrd.py

---
+++ Python/2.keywords/6.structurekywrd.py
@@ -0,0 +1,14 @@
+#Structure Keywords: def, class, with, as, pass, lambda
+
+#The def keyword is used to define user-defined funtion or methods of a class
+
+# def keyword: create function
+def addition(num1, num2):
+    print('Sum is', num1 + num2)
+
+# call function
+addition(10, 20)
+
+Output:
+
+Sum is 30
