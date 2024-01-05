5f47521cce81653e15ee4f750daa9e74954fe051
Roshan Palaniappan <138195422+RODEEKS@users.noreply.github.com>
2024-01-05 11:12:58 +0530

    5.iterativekywrd.py

---
+++ Python/2.keywords/5.iterativekywrd.py
@@ -0,0 +1,21 @@
+#Iterative and Transfer Keywords: for, while, break, continue, else
+
+print('for loop to display first 5 numbers')
+for i in range(5):
+    print(i, end=' ')
+
+print('while loop to display first 5 numbers')
+n = 0
+while n < 5:
+    print(n, end=' ')
+    n = n + 1
+
+Output:
+
+for loop to display first 5 numbers
+0 1 2 3 4
+
+while loop to display first 5 numbers
+0 1 2 3 4
+
+#break, continue, and pass: In Python, transfer statements are used to alter the program’s way of execution in a certain manner.
