d1fcc9d8ce9d0a9c64600640b566ced6a40f2688
Roshan Palaniappan <138195422+RODEEKS@users.noreply.github.com>
2024-01-05 11:17:45 +0530

    8.returnkywrd.py

---
+++ Python/2.keywords/8.returnkywrd.py
@@ -0,0 +1,12 @@
+'''In Python, to return value from the function, a return statement is used.
+yield is a keyword that is used like return, except the function will return a generator.'''
+
+def addition(num1, num2):
+    return num1 + num2  # return sum of two number
+
+# call function
+print('Sum:', addition(10, 20))
+
+Output:
+Sum: 30
+
