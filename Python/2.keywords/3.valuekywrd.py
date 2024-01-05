77df9d7befeef8aebe0151dcbb64ff2e04eebab8
Roshan Palaniappan <138195422+RODEEKS@users.noreply.github.com>
2024-01-05 11:05:27 +0530

    3.valuekywrd.py

---
+++ Python/2.keywords/3.valuekywrd.py
@@ -0,0 +1,10 @@
+'''
+True and False are used to represent truth values, know as boolean values. 
+It is used with a conditional statement to determine which block of code to execute. When executed, the condition evaluates to True or False.
+'''
+
+x = 25
+y = 20
+
+z = x > y
+print(z)  # True
