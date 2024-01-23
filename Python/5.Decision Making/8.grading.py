fd2b1704285b22d6b76efa2c9c444590090ae5fa
Roshan Palaniappan <138195422+RODEEKS@users.noreply.github.com>
2024-01-23 11:57:40 +0530

    8.grading.py

---
+++ Python/5.Decision Making/8.grading.py
@@ -0,0 +1,13 @@
+
+grade=int(input("Enter number\n"))
+
+if grade>100:
+    print("Invalid")
+elif grade>90:
+    print("A")
+elif grade>75:
+    print("B")
+elif grade>55:
+    print("C")
+else:
+    print("D")
