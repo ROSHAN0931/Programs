a0ae141cecea241c24f36db9a7b0a6937c9a3b44
Roshan Palaniappan <138195422+RODEEKS@users.noreply.github.com>
2024-01-23 11:50:03 +0530

    4.minimumof3numbers.py

---
+++ Python/5.Decision Making/4.minimumof3numbers.py
@@ -0,0 +1,11 @@
+
+a=int(input("Enter number\n"))
+b=int(input("Enter number\n"))
+c=int(input("Enter number\n"))
+
+if a<b and a<c:
+    print("a is small")
+elif b<a and b<c:
+    print("b is small")
+else:
+    print("c is small")
