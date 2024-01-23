a45598a45f1406cb01e5f16a3f0d61386553e8f0
Roshan Palaniappan <138195422+RODEEKS@users.noreply.github.com>
2024-01-23 11:52:52 +0530

    6.maximumof3numbers.py

---
+++ Python/5.Decision Making/6.maximumof3numbers.py
@@ -0,0 +1,11 @@
+
+a=int(input("Enter number\n"))
+b=int(input("Enter number\n"))
+c=int(input("Enter number\n"))
+
+if a>b and a>c:
+    print("a is big")
+elif b>a and b>c:
+    print("b is big")
+else:
+    print("c is big")
