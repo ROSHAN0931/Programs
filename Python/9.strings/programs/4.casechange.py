e44f8ac231e74f38b03dd2f6534476a9dc848ed4
Roshan Palaniappan <138195422+RODEEKS@users.noreply.github.com>
2024-03-01 09:13:27 +0530

    4.casechange.py

--- Python/9.strings/programs/4.casechange.py
+++ Python/9.strings/programs/4.casechange.py
@@ -1,7 +1,5 @@
 # Write a program that takes a sentence and then converts all the characters to uppercase.
 
 a=input("Enter string : ")
-for i in a:
-    if a.lower():
-        a=a.upper()
-print(a)
+
+print(a.swapcase())
